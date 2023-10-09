5fd526c7f3de72f5e579953fe1c3373fdf49ac75
Eliezer Caliri <lirinhojc@gmail.com>
2023-10-09 03:08:45 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,10 @@
+const PrimeiroNumero = 5;
+const SegundoNumero = 3 ;
+let cadastroAtivo = true;
+
+console.log(PrimeiroNumero === SegundoNumero)
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2 )
---
+++ type-number.js
@@ -0,0 +1,8 @@
+const MeuNumero = 3;
+
+const PrimeiroNumero = 1;
+const SegundoNumero = 2;
+
+const operacaoMatematica = PrimeiroNumero + SegundoNumero;
+
+console.log(operacaoMatematica)
